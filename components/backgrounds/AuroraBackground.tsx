d30bcdd8922770f038ae1dbfd14dcc07acc2a572
Onepiecedad <joakim123@gmail.com>
2025-04-09 12:45:06 +0000

    Fix aurora background and prepare for production

--- components/backgrounds/AuroraBackground.tsx
+++ components/backgrounds/AuroraBackground.tsx
@@ -26,7 +26,7 @@ export const AuroraBackground = ({
   }
 
   return (
-    <main className={cn('fixed inset-0 -z-10 overflow-hidden bg-zinc-50 dark:bg-zinc-900', className)}>
+    <main className={cn('fixed inset-0 -z-10 overflow-hidden bg-zinc-900', className)}>
       <div className="absolute inset-0 overflow-hidden">
         <div
           className={cn(
@@ -38,14 +38,8 @@ export const AuroraBackground = ({
             [background-size:300%,_200%]
             [background-position:50%_50%,50%_50%]
             filter blur-[10px] invert dark:invert-0
-            after:content-[""] after:absolute after:inset-0 after:[background-image:var(--white-gradient),var(--aurora)] 
-            after:dark:[background-image:var(--dark-gradient),var(--aurora)]
-            after:[background-size:200%,_100%] 
-            after:animate-aurora after:[background-attachment:fixed] after:mix-blend-difference
             pointer-events-none
-            absolute -inset-[10px] opacity-50 will-change-transform`,
-            showRadialGradient &&
-              `[mask-image:radial-gradient(ellipse_at_100%_0%,black_10%,var(--transparent)_70%)]`
+            absolute -inset-[10px] opacity-50 will-change-transform`
           )}
         />
       </div>
